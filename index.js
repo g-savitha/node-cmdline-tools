5008adac7ee16095799163e0d6d9a10d43ce2599
Savitha <savitha.gollamudi@gmail.com>
2020-07-12 17:37:54 +0530

    fixed ordering -callback approach

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const fs = require("fs");
 
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) throw new Error(err);
+  let allStats = Array(filenames.length).fill(null);
 
   for (let file of filenames) {
+    const idx = filenames.indexOf(file);
     fs.lstat(file, (err, stats) => {
       if (err) throw new Error(err);
 
-      if (stats.isFile()) console.log("file :", file);
-      if (stats.isDirectory()) console.log("directory :", file);
+      allStats[idx] = stats;
+      // returns true when none of the elements in array are null
+      const ready = allStats.every((stats) => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, idx) => {
+          console.log(filenames[idx], stats.isFile());
+        });
+      }
     });
+    console.log();
   }
 });
